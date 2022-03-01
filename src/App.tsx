8ce50f55fe81490da61e6390d00baaf42a9a35a6
Mushfiqus Salehin <musfiqus@gmail.com>
2022-03-02 01:24:58 +0600

    add: yarn package manager

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi! I am <code>Mushfiqus Salehin</code>
+          Hi! I'm  <code>Mushfiqus Salehin</code>
         </p>
         <a
           className="App-link"
